b6226972375070ef03a433cfbf839f78947f5dcd
Aleksandr Savchenko <titamoto@gmail.com>
2023-03-21 10:04:47 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function receivesAFunction(callback) {
+    callback() ;
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {};
+}
+
+function returnsAnAnonymousFunction() {
+    return () => {};
+};
